9777c82a37b2beadd260f301caf4cb38b1154206
C36-HussainHaidarAlmousawi <70634196+C36-HussainHaidarAlmousawi@users.noreply.github.com>
2020-10-10 21:41:45 +0300

    done

---
+++ IOSClasswork8/iOSClasswork8/ViewController/cityTVCe.swift
@@ -0,0 +1,27 @@
+//
+//  cityTVCe.swift
+//  iOSClaswork8
+//
+//  Created by hussain haidar almousawi on 10/10/20.
+//  Copyright © 2020 Safeyah Coding. All rights reserved.
+//
+
+import UIKit
+
+class cityTVCe: UITableViewCell {
+    @IBOutlet weak var Img: UIImageView!
+    
+    @IBOutlet weak var Lbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
