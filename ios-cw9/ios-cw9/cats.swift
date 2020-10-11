7d38e8e0db6dfd86cb5aafe10605d983a69a8f84
C36-HussainHaidarAlmousawi <70634196+C36-HussainHaidarAlmousawi@users.noreply.github.com>
2020-10-11 22:25:40 +0300

    done

---
+++ ios-cw9/ios-cw9/cats.swift
@@ -0,0 +1,10 @@
+//
+//  cats.swift
+//  ios-cw9
+//
+//  Created by hussain haidar almousawi on 10/10/20.
+//
+
+import Foundation
+
+var cats = ["cute" , "sleeeeeep" , "patrone" , "lion" , "covid-19" , "mst7y" , "monster" , "baby" , "anime" , "beautiful eyes"]
---
+++ ios-cw9/ios-cw9/catsTVCe.swift
@@ -0,0 +1,28 @@
+//
+//  catsTVCe.swift
+//  ios-cw9
+//
+//  Created by hussain haidar almousawi on 10/10/20.
+//
+
+import UIKit
+
+class catsTVCe: UITableViewCell {
+
+    @IBOutlet weak var Img: UIImageView!
+    
+    
+    @IBOutlet weak var Label: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
